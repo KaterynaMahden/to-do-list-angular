769ba90c209e16797d14159c5f3ea9b51240f12a
KaterynaMahden <79214759+KaterynaMahden@users.noreply.github.com>
2022-09-27 17:12:20 +0300

    ngxs integration

--- src/app/states/app.state.ts
+++ src/app/states/app.state.ts
@@ -1,17 +1,49 @@
 import { Injectable } from "@angular/core";
+import { Action, createSelector, State, StateContext, Store } from "@ngxs/store";
+import { Item } from "../item";
 
-export class ItemsStateModel {
-    items: []
+
+export interface ItemsStateModel {
+    modal: boolean;
 }
 
-@State<ItemsStateModel>({
-    name: 'itemsstate',
-    defaults: {
-        items: []
-    }
-})
+export class AllItems {
+    static readonly type = '[Items] AllItems';
+    constructor(public itemsState: ItemsStateModel,
+        private store: Store,
+        ) {}
+}
+
+// @State<string[]>({
+//     name: 'items',
+//     defaults: { 
+//         itemsAll: []
+//     }
+// })
 
 @Injectable()
-export class AppState {
-    constructor(private:)
+// export class ItemsState {
+
+//     @Action(AllItems) 
+//     allItems(ctx: StateContext<ItemsStateModel>, action: AllItems){
+//         const state = ctx.getState();
+//         ctx.setState({
+//             ...state,
+//             itemsAll: [
+//                 ...state.items,
+//                 action.itemsState
+//             ]
+//         })
+//     }
+    
+// }
+
+export class ItemsState {
+    static getItems(item: Item) {
+        return createSelector(
+            [ItemsState],
+            (items:Item[]) => items.filter(item => item.id === item)
+        )
+    }
 }
+
